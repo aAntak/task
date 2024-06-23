58e0ad22cc885031d274b3704a3cddc654044600
Andrius Kondratjev <andrius.kondratjev@devbridge.com>
2024-06-23 20:49:18 +0300

    Add truncate for long link

--- src/modules/core/components/actions/link.tsx
+++ src/modules/core/components/actions/link.tsx
@@ -29,11 +29,13 @@ const Link = ({
       aria-disabled={disabled}
       rel={target === LinkTarget.Blank ? 'noopener' : undefined}
       onClick={onClick}
-      className={`inline-flex items-center justify-center rounded bg-purple py-1.5 py-2 px-3 ${
+      className={`inline-flex items-center justify-center rounded bg-purple py-1.5 py-2 px-3 min-w-0 ${
         disabled ? 'opacity-40 pointer-events-none' : ''
       } ${fullWidth ? 'w-full' : ''}`}
     >
-      <span className="text-subtitle-medium text-grey-95">{children}</span>
+      <span className="text-subtitle-medium text-grey-95 truncate">
+        {children}
+      </span>
     </a>
   );
 };
--- src/modules/core/components/page/header.tsx
+++ src/modules/core/components/page/header.tsx
@@ -7,9 +7,9 @@ type HeaderProps = {
 
 const Header = ({ leftContent, rightContent }: HeaderProps) => {
   return (
-    <header className="flex items-center justify-between md:justify-end pr-2.5 pl-1 h-14 shrink-0 border-b border-black-18">
+    <header className="flex items-center justify-between md:justify-end pr-2.5 pl-1 h-14 shrink-0 border-b border-black-18 gap-1.5">
       <div>{leftContent}</div>
-      <div>{rightContent}</div>
+      <div className="flex min-w-0">{rightContent}</div>
     </header>
   );
 };
